326379ee6ad53122796d51851769572c37e320ae
bh <blownhitherma@gmail.com>
2016-12-01 04:02:05 +0800

    try threading with GreenPool

---
+++ network/client.py
@@ -0,0 +1,24 @@
+import socket
+import config
+
+def read_msg(server):
+    print("read")
+    msg_len = server.recv(1)
+    print(msg_len)
+    try:
+        msg_len = int(msg_len)
+    except Exception:
+        msg_len = 32
+    msg = server.recv(msg_len)
+    return str(msg, 'utf-8')
+
+
+if __name__ == "__main__":
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.connect((config.HOST, config.PORT))
+    print("Connected")
+    while True:
+        msg = input("Msg: ")
+        server.send(bytes(msg, "utf-8"))
+        ans = read_msg(server)
+        print(ans)

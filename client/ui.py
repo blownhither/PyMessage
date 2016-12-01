fa19d9281841cac576cb6325e4ba8b30f035e064
Chen Kan <zimisu@gmail.com>
2016-12-01 21:39:38 +0800

    now client can send msg

--- client/ui.py
+++ client/ui.py
@@ -6,8 +6,9 @@ from PyQt5 import QtGui, Qt
 from PyQt5.QtCore import QCoreApplication, QTimer
 from PyQt5.QtGui import QFont
 from PyQt5.QtWidgets import *
-
 from client.util import *
+import socket
+import json, pub_config
 
 
 class MainWindow(QMainWindow):
@@ -92,12 +93,22 @@ class MainWidget(QWidget):
         file_menu.addAction(exitAction)
 
     def enterGroup(self):
-        self.chat = Chat_Box(username=self.username, group_id=self.group_id.text())
-        self.group_id.setText('')
+        if self.group_id.text() == '':
+            return
+        self.chat = Chat_Box.create_chat_box(username=self.username, group_id=self.group_id.text())
+        if self.chat.sock is None:
+            self.chat = None
 
-        self.chat.show_msg('mzy', '你好哇..................................................')
-        for i in range(5):
-            self.chat.show_msg('ck', '你好哇， 傻逼')
+        if self.chat is not None:
+            self.chat.show_msg('mzy', '你好哇..................................................')
+            for i in range(5):
+                self.chat.show_msg('ck', '你好哇， 傻逼')
+        else:
+            msg = QMessageBox()
+            msg.setWindowTitle('Connect Failed')
+            msg.setText("Group ID: %s \nCan't connect to server now, please try later." % self.group_id.text())
+            msg.exec_()
+        self.group_id.setText('')
 
     def refresh_friend_list(self):
         # timer = QTimer()
@@ -106,8 +117,17 @@ class MainWidget(QWidget):
 
 
 class Chat_Box(QWidget):
+    __opened_groups__ = set()
+
+    @classmethod
+    def create_chat_box(cls, username=None, group_id=None):
+        if group_id in cls.__opened_groups__ or group_id is None or username is None:
+            return None
+        return Chat_Box(username, group_id)
+
     def __init__(self, username='Me', group_id=None):
         super().__init__()
+        self.__opened_groups__.add(group_id)
         self.username = username
         self.group_id = group_id
         self.setGeometry(620, 100, 700, 400)
@@ -161,13 +181,55 @@ class Chat_Box(QWidget):
 
         self.show()
 
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        try_count = 5
+        success = False
+        while try_count > 0 and not success:
+            try_count -= 1
+            try:
+                self.sock.connect(('127.0.0.1', 8888))
+                success = True
+            except Exception as e:
+                print('Connect failed')
+        if not success:
+            self.sock = None
+
+    def connect_handler(self):
+        try:
+            while True:
+                data = self.sock.recv(65536)
+                data = json.loads(data)
+                if data['type'] == pub_config.SERVER_SEND_MSG:
+                    self.show_msg(user=data['sender'], content=data['content'])
+                elif data['type'] == pub_config.RETURN_GROUP_MEMBERS:
+                    self.refresh_member_list(data['members'])
+        except Exception as e:
+            self.sock.close()
+
+    def refresh_member_list(self, member_list):
+        self.friends_list.clear()
+        self.friends_list.addItems(member_list)
+        pass
+
     def show_msg(self, user, content):
+        if user == self.username:
+            return
         self.text_box.append('%s  %s:\n%s' % (user, get_time_str(), content))
         self.text_box.append('')
 
     def send_msg(self):
+        data = {
+            'type': pub_config.CLIENT_SEND_MSG,
+            'sender': self.username,
+            'content': self.input_box.toPlainText()
+        }
+        data = json.dumps(data)
+        self.sock.send(data)
         self.show_msg(self.username, self.input_box.toPlainText())
         self.input_box.setText('')
+
+    def __del__(self):
+        self.__opened_groups__.remove(self.group_id)
 
 
 if __name__ == '__main__':
